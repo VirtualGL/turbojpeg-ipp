5fe8e02f0e303f89c9d4b7d2fad2419d19f75b83
DRC <dcommander@users.sourceforge.net>
2008-04-03 18:43:12 +0000

    Handle pathnames w/ spaces
    git-svn-id: svn+ssh://svn.code.sf.net/p/virtualgl/code/turbojpeg-ipp/trunk@2052 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- switchtjpeg.bat
+++ switchtjpeg.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set TJIPP=%~d0%~p0turbojpeg-ipp.dll
-set TJLIBJPEG=%~d0%~p0turbojpeg-libjpeg.dll
-set TJDEST=%~d0%~p0turbojpeg.dll
+set TJIPP="%~d0%~p0turbojpeg-ipp.dll"
+set TJLIBJPEG="%~d0%~p0turbojpeg-libjpeg.dll"
+set TJDEST="%~d0%~p0turbojpeg.dll"
 
 if /i "%1"=="ipp" goto ipp
 if /i "%1"=="libjpeg" goto libjpeg
